520a9a283639197c010ba8ecab83b053ca065339
Philip Ivanov <philip.i@outlook.com>
2018-06-26 23:27:40 +0300

    BruteCollinearPoints blank structure, LineSegment and Readme

---
+++ Pattern Recognition/BruteCollinearPoints.java
@@ -0,0 +1,25 @@
+/**
+ * Examines 4 points at a time and checks whether they all lie on the same line segment,
+ * returning all such line segments.
+ *
+ * Created by Philip Ivanov (https://github.com/personalaccount)
+ */
+
+
+public class BruteCollinearPoints {
+
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+
+    }
+
+    // the line segments
+    public LineSegment[] segments(){
+
+    }
+}
---
+++ Pattern Recognition/README.md
@@ -0,0 +1,12 @@
+# Programming Assignment 3: Pattern Recognition
+
+> Please navigate to the official [assignment 3 URL](http://coursera.cs.princeton.edu/algs4/assignments/collinear.html) for details.
+
+Write a program to recognize line patterns in a given set of points.
+
+**The problem.** Given a set of n distinct points in the plane, find every (maximal) line segment that connects a subset of 4 or more of the points.
+
+**Point data type.** Immutable data type that represents a point in the plane.
+
+
+
