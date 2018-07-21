a2f46406e943f09b703f84e29d8c473aa7976547
Philip Ivanov <philip.i@outlook.com>
2018-07-21 23:45:42 +0300

    SlopeOrder innerclass refactoring

--- Pattern Recognition/Point.java
+++ Pattern Recognition/Point.java
@@ -7,12 +7,12 @@
 
 import java.util.Comparator;
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Point implements Comparable<Point> {
 
     private final int x;     // x-coordinate of this point
     private final int y;     // y-coordinate of this point
-    public final Comparator<Point> BY_SLOPE = new SlopeOrder();
 
     /**
      * Initializes a new point.
@@ -98,25 +98,22 @@ public class Point implements Comparable<Point> {
      *
      * @return the Comparator that defines this ordering on points
      */
-    public Comparator<Point> slopeOrder() {
+    public Comparator<Point> slopeOrder(Point q1, Point q2) {
 
         /* YOUR CODE HERE */
-        return
+        return new SlopeOrder();
         //returns a Comparator Object
     }
 
-    // Define a nested class that implements the Comparator interface
-    private class SlopeOrder implements Comparator<Point>
-    {
-        public int compare(Point q1, Point q2){
-            if slopeTo(q1) >
-            double dy1 = q1.y - y;
-            double dy2 = q2.y - y;
+    // Define a nested inner class that implements the Comparator interface
+    private class SlopeOrder implements Comparator<Point> {
+        public int compare(Point q1, Point q2) {
+            double slopeq1 = slopeTo(q1);
+            double slopeq2 = slopeTo(q2);
 
-            if (dy1 == 0 && dy2 == 0) {
-                else if (dy1 >= 0 && dy2 < 0) return -1;
-                else if (dy2 >= 0 && dy1 < 0) return +1;
-                else return -ccw(Point.this, q1, q2);
+            if (slopeq1 > slopeq2) return +1;
+            if (slopeq1 < slopeq2) return -1;
+            return 0;
         }
     }
 
@@ -136,6 +133,7 @@ public class Point implements Comparable<Point> {
      * Unit tests the Point data type.
      */
     public static void main(String[] args) {
+        StdOut.println("Launching Point");
         /* YOUR CODE HERE */
     }
 }
