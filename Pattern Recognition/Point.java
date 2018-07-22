cf2aa22a6d3fb03f560bcc5986ef785d742c429f
Philip Ivanov <philip.i@outlook.com>
2018-07-22 23:07:33 +0300

    Unit tests

--- Pattern Recognition/Point.java
+++ Pattern Recognition/Point.java
@@ -135,14 +135,9 @@ public class Point implements Comparable<Point> {
     public static void main(String[] args) {
 
         StdOut.println("Create three points");
+        Point[] points = {new Point(0,0), new Point(3,0), new Point(3,3)};
 
-        // Create a point at 0,0
-        Point p1 = new Point(0,0);
-        StdOut.print(p1.toString());
-
-        Point p2 = new Point(0,3);
-        Point p3 = new Point(3,3);
-
-        /* YOUR CODE HERE */
+        for(Point p : points){ StdOut.print(p.toString()); }
+        
     }
 }
