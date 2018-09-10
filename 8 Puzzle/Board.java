b0e606cb29d6b9ead7344409bd9e56ba18e6b2a0
Philip Ivanov <philip.i@outlook.com>
2018-09-10 23:52:10 +0300

    readme

--- 8 Puzzle/Board.java
+++ 8 Puzzle/Board.java
@@ -3,4 +3,14 @@
  */
 public class Board {
 
+    // construct a board from an n-by-n array of blocks
+    // (where blocks[i][j] = block in row i, column j)
+
+    public Board(int[][] blocks){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
