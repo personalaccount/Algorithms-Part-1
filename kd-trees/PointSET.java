9d08691aabb3673663866ede22d6f3176bbf4f41
Philip Ivanov <philip.i@outlook.com>
2018-11-17 22:51:48 +0300

    PointSET update

--- kd-trees/PointSET.java
+++ kd-trees/PointSET.java
@@ -12,7 +12,7 @@ public class PointSET {
 
     // is the set empty?
     public boolean isEmpty() {
-        return true;
+        return (points.length == 0);
     }
 
     // number of points in the set
