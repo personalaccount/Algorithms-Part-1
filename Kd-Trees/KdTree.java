2f668c247aa19709cfe3921c7ccf3ff314aa37da
Philip Ivanov <philip.i@outlook.com>
2018-12-08 00:07:57 -0500

    progress updates

--- Kd-Trees/KdTree.java
+++ Kd-Trees/KdTree.java
@@ -11,39 +11,51 @@ import java.util.Stack;
 
 public class KdTree {
 
-    // Array holding a bst
-    private double[] tree;
+    // Array holding a bst of points.
+    private Point2D[] tree;
 
-    // construct an empty set of points
+    // Construct an empty set of points
     public KdTree() {
-
-        // Empty array of size 3
-        tree = new double[3];
+        // Empty array of size 0
+        tree = new Point2D[0];
     }
 
+    // Auxiliary method to check if the point is null
     private void exceptionIfNull(Point2D p) {
         if (p == null) throw new IllegalArgumentException();
     }
 
+    // Auxilliary method to check if the point is to the right of this one
+
     public boolean isEmpty() {
-        return points.isEmpty();
+        return (tree.length == 0);
     }
 
     // Number of points in the set
     public int size() {
-
+        return tree.length;
     }
 
-    // Add the point to the set (if it is not already in the set)
+    // Insert the point into the set (if it is not already in the set)
     public void insert(Point2D p) {
         exceptionIfNull(p);
-        if (!contains(p)) points.add(p);
+
+        // If this is the first insert increase array size to 3 and insert the point at the root
+        if (isEmpty()) {
+            resize(3);
+            tree[0] = p;
+        }
+//        if (!contains(p)) add(p);
     }
+
+    private void add(Point2D p) {
+
+    }
+
 
     // Does the set contain point p?
     public boolean contains(Point2D p) {
         exceptionIfNull(p);
-        return points.contains(p);
     }
 
     // draw all points to standard draw
